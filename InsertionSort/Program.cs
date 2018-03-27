c510900ca9de83882b9576cfa18e4146a1a05f1b
Sourav Kundu <souravkundu98@gmail.com>
2018-03-27 19:14:12 +0530

    Add project files.

---
+++ InsertionSort/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 12, 11, 13, 5, 6 };
+
+            Sort(arr);
+
+            PrintArray(arr);
+        }
+
+        static void Sort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 1; i < n; ++i)
+            {
+                int key = arr[i];
+                int j = i - 1;
+
+                /* Move elements of arr[0..i-1], that are
+                   greater than key, to one position ahead
+                   of their current position */
+                while (j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+                arr[j + 1] = key;
+            }
+        }
+
+
+        // Prints the array
+        static void PrintArray(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; ++i)
+                Console.Write(arr[i] + " ");
+            Console.WriteLine("\n");
+        }
+    }
+}
+
+
+//https://www.geeksforgeeks.org/insertion-sort/
+//https://www.khanacademy.org/computing/computer-science/algorithms/insertion-sort/a/insertion-sort
---
+++ SelectionSort/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 64, 25, 12, 22, 11 };
+            PrintArray(arr);
+            Sort(arr);
+            Console.WriteLine("Sorted array");
+            PrintArray(arr);
+        }
+
+        static void Sort(int[] arr)
+        {
+            int n = arr.Length;
+
+            // One by one move boundary of unsorted subarray
+            for (int i = 0; i < n - 1; i++)
+            {
+                // Find the minimum element in unsorted array
+                int min_idx = i;
+                for (int j = i + 1; j < n; j++)
+                    if (arr[j] < arr[min_idx])
+                        min_idx = j;
+
+                // Swap the found minimum element with the first element
+                Swap(arr, min_idx, i);
+            }
+        }
+
+
+        // Prints the array
+        static void PrintArray(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; ++i)
+                Console.Write(arr[i] + " ");
+            Console.WriteLine("\n");
+        }
+
+        static void Swap(int[] arr, int x, int y)
+        {
+            int temp = arr[x];
+            arr[x] = arr[y];
+            arr[y] = temp;
+        }
+    }
+}
+
+
+//https://www.geeksforgeeks.org/selection-sort/
+//https://www.khanacademy.org/computing/computer-science/algorithms/sorting-algorithms/a/analysis-of-selection-sort
+/*
+ * 
+ * Time Complexity: O(n2) as there are two nested loops.
+
+Auxiliary Space: O(1)
+The good thing about selection sort is it never makes more than O(n) swaps and can be useful when memory write is a costly operation.
+ 
+     */
